edb72f219c06cf192ef71cf407e34709aba09c1b
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-08-19 14:13:40 -0300

    Changing tools menu, change language and zoom control component styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -55,4 +55,17 @@ export default createGlobalStyle`
     color: #1f5582;
   }
 
+  .zoom-controls {
+    background: #ffffff;
+    box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.13), 1px 2px 2px rgba(0, 0, 0, 0.1),
+      -1px -2px 2px rgba(0, 0, 0, 0.05);
+  }
+
+  .zoom-controls {
+    &:hover {
+      background: #ffffff;
+    }
+
+  }
+
 `;
