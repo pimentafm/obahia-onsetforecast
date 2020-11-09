a27323f87b135c68edf7c9e66a061a89506d46df
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-11-09 14:01:05 -0300

    Updating GA track code

--- src/pages/Region/index.tsx
+++ src/pages/Region/index.tsx
@@ -6,7 +6,7 @@ import ReactGA from 'react-ga';
 
 const Region: React.FC = () => {
   useEffect(() => {
-    ReactGA.initialize('UA-182410588-3');
+    ReactGA.initialize('UA-182410588-6');
     ReactGA.pageview('/');
   }, []);
   return <Map defaultYear={2018} defaultCategory="Regional" />;
